bf26e153b57228387d4462b27c6720cc5fa7e9b6
TomCat <midoelmelegy@gmail.com>
2023-01-03 19:24:11 +0800

    First commit

---
+++ src/components/Aggregator/constants.ts
@@ -0,0 +1,38 @@
+export const seaportReferrerAddress = '0x4c31e558393312a1d3bE14C45A3656A2e915F53D';
+
+export const chainsMap = {
+	ethereum: 1,
+	bsc: 56,
+	polygon: 137,
+	optimism: 10,
+	arbitrum: 42161,
+	avax: 43114,
+	gnosis: 100,
+	fantom: 250,
+	klaytn: 8217,
+	aurora: 1313161554,
+	celo: 42220,
+	cronos: 25,
+	dogechain: 2000,
+	moonriver: 1285,
+	bttc: 199,
+	oasis: 42262,
+	velas: 106,
+	heco: 128,
+	harmony: 1666600000,
+	boba: 288,
+	okexchain: 66,
+	fuse: 122,
+	moonbeam: 1284
+};
+
+export const chainNamesReplaced = {
+	bsc: 'BSC',
+	avax: 'Avalanche',
+	okexchain: 'OKX',
+	bttc: 'BitTorrent'
+};
+
+export const nativeAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'.toLowerCase();
+
+export const initialLiquidity = [500, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 500_000_000];
