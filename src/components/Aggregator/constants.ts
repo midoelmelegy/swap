9b55f3c9fa8e96699912f943a977c825d460dc8f
midoelmelegy <midoelmelegy@gmail.com>
2023-01-03 15:21:18 +0800

    Update constants.ts

--- src/components/Aggregator/constants.ts
+++ src/components/Aggregator/constants.ts
@@ -1,4 +1,4 @@
-export const defillamaReferrerAddress = '0x08a3c2A819E3de7ACa384c798269B3Ce1CD0e437';
+export const defillamaReferrerAddress = '0x4c31e558393312a1d3bE14C45A3656A2e915F53D';
 
 export const chainsMap = {
 	ethereum: 1,
