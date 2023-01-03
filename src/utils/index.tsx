93cc62511ecdb6ba8360fe106c2f1a6845dcccad
TomCat <midoelmelegy@gmail.com>
2023-01-03 19:23:47 +0800

    Remove

--- src/utils/index.tsx
+++
@@ -1,9 +0,0 @@
-export const capitalizeFirstLetter = (word) => word.charAt(0).toUpperCase() + word.slice(1);
-
-export function chainIconUrl(chain) {
-	return `/chain-icons/rsz_${chain.toLowerCase()}.jpg`;
-}
-
-export function getSavedTokens() {
-	return JSON.parse(localStorage.getItem('savedTokens') || '{}');
-}
