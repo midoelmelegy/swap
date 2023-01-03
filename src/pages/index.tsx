db2cdf2a99fe2aa71abe78dd94e26d43b2bd0f97
TomCat <midoelmelegy@gmail.com>
2023-01-03 15:46:29 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export async function getStaticProps() {
 
 export default function Aggregator(props) {
 	return (
-		<Layout title={`Meta-dex aggregator - SeaPort`} defaultSEO>
+		<Layout title={`SeaPort Swap`} defaultSEO>
 			<ConnectButton />
 			<AggregatorContainer tokenlist={props.tokenlist} />
 		</Layout>
